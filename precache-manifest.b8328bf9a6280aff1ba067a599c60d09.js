09d3b8fc42731072c56404b788ad8be9d4b6619d
Abimael Santos <juanabimael@gmail.com>
2020-10-29 04:51:18 -0500

    Updates

---
+++ precache-manifest.b8328bf9a6280aff1ba067a599c60d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72956715077059fe37b47b526f389d58",
+    "url": "/what-s-in-there/index.html"
+  },
+  {
+    "revision": "d0b212e13e1b9adc15c8",
+    "url": "/what-s-in-there/static/css/main.f7978d98.chunk.css"
+  },
+  {
+    "revision": "5c77e51c829ea46097e4",
+    "url": "/what-s-in-there/static/js/2.0f50c07d.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/what-s-in-there/static/js/2.0f50c07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b212e13e1b9adc15c8",
+    "url": "/what-s-in-there/static/js/main.6d6957e7.chunk.js"
+  },
+  {
+    "revision": "076acc7b7363143b143b",
+    "url": "/what-s-in-there/static/js/runtime-main.9315fa11.js"
+  }
+]);
